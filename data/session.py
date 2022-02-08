98acc38ee472f32629dda52cc0c0ade7c3b6dd91
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-02-08 14:44:24 +0300

    add check_email_free

--- data/session.py
+++ data/session.py
@@ -8,6 +8,10 @@ class Session:
         db_session.global_init('./db/db.db')
         self.session = db_session.create_session()
 
-    def check_login(self, login) -> bool:
+    def check_login_free(self, login) -> bool:
         user = self.session.query(User).filter(User.login == login).first()
-        return user is not None
+        return user is None
+
+    def check_email_free(self, email) -> bool:
+        user = self.session.query(User).filter(User.email == email).first()
+        return user is None

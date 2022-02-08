eeab4aff27af0c7ffd45409827f5184eef891404
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-02-08 14:42:23 +0300

    add check_login

--- data/session.py
+++ data/session.py
@@ -1,4 +1,5 @@
-from data import user, db_session
+from data import db_session
+from user import User
 
 
 class Session:
@@ -6,3 +7,7 @@ class Session:
     def __init__(self):
         db_session.global_init('./db/db.db')
         self.session = db_session.create_session()
+
+    def check_login(self, login) -> bool:
+        user = self.session.query(User).filter(User.login == login).first()
+        return user is not None

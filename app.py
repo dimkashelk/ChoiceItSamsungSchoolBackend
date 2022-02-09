1be37feb9a0624d0300fbbea10affcb82c54a0ef
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-02-09 16:23:23 +0300

    add checking parameters of methods

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 session = Session()
 
 
+def presence_of_arguments(content, *args):
+    for i in args:
+        if not content.get(i, False):
+            return False
+    return True
+
+
 @app.route('/api/')
 def hello_world():
     return 'Hello World!'
@@ -13,24 +20,32 @@ def hello_world():
 @app.route('/api/check_login')
 def check_login():
     content = request.json
-    return jsonify({'is_free_login': session.check_login_free(content['login'])})
+    if presence_of_arguments(content, 'login'):
+        return jsonify({'is_free_login': session.check_login_free(content['login'])})
+    else:
+        return jsonify({'status': False})
 
 
 @app.route('/api/check_email')
 def check_email():
     content = request.json
-    return jsonify({'is_free_email': session.check_email_free(content['email'])})
+    if presence_of_arguments(content, 'email'):
+        return jsonify({'is_free_email': session.check_email_free(content['email'])})
+    else:
+        return jsonify({'status': False})
 
 
 @app.route('/api/auth')
 def check_data():
     content = request.json
-    if content.get('email', False):
+    if presence_of_arguments(content, 'email'):
         print(f"GETTING email: {content['email']} password: {content['password']} TO CHECKING AUTH")
         res = session.authorization(content['email'], content['password'])
-    else:
+    elif presence_of_arguments(content, 'login'):
         print(f"GETTING login: {content['login']} password: {content['password']} TO CHECKING AUTH")
         res = session.authorization(content['login'], content['password'])
+    else:
+        return jsonify({'status': False})
     if res is None:
         return jsonify({
             'ok': False
@@ -45,12 +60,19 @@ def check_data():
 @app.route('/api/find_email')
 def find_email():
     content = request.json
-    return jsonify({'find': session.find_email(content['email'])})
+    if presence_of_arguments(content, 'email'):
+        return jsonify({'find': session.find_email(content['email'])})
+    else:
+        return jsonify({'status': False})
 
 
 @app.route('/api/check_verify_code')
 def check_verify_code():
     content = request.json
+    if not presence_of_arguments(content, 'email', 'code', 'password'):
+        return jsonify({
+            'ok': False
+        })
     res = session.check_verify_code(
         content['email'],
         content['code'],
@@ -70,6 +92,10 @@ def check_verify_code():
 @app.route('/api/reg')
 def reg():
     content = request.json
+    if not presence_of_arguments(content, 'email', 'first_name', 'password', 'login', 'second_name'):
+        return jsonify({
+            'status': False
+        })
     res = session.registration(
         content['login'],
         content['password'],
